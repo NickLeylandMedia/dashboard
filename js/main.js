1a49882678a62bf918f09e217a7ac16886be1750
NickLeylandMedia <nick@nick-leyland.com>
2021-02-15 11:48:52 -0500

    Added styleswitcher.js from designshack.net/articles/css-style-switcher, added red/black and red/teal stylesheets.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Collapse tabs (resources.html):
 function collapseCategory() {
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -14,3 +15,31 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 }
+
+//To-Do List:
+let todoItems = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+};
+
+todoItems.push(todo);
+console.log(todoItems);
+}
+
+const form =document.querySelector('.js-form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input =document.querySelector('.js-todo-input');
+
+  const text =input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value ='';
+    input.focus();
+  }
+})
