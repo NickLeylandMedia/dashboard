c387238a4f5da46c3a0bbdcc55299b707384365c
NickLeylandMedia <nick@nick-leyland.com>
2021-02-16 09:00:29 -0500

    Began coding to-do list app

--- js/main.js
+++ js/main.js
@@ -17,29 +17,6 @@ for (i = 0; i < coll.length; i++) {
 }
 
 //To-Do List:
-let todoItems = [];
+const listsContainer =document.querySelector('[data-lists]')
 
-function addTodo(text) {
-  const todo = {
-    text,
-    checked: false,
-    id: Date.now(),
-};
-
-todoItems.push(todo);
-console.log(todoItems);
-}
-
-const form =document.querySelector('.js-form');
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const input =document.querySelector('.js-todo-input');
-
-  const text =input.value.trim();
-  if (text !== '') {
-    addTodo(text);
-    input.value ='';
-    input.focus();
-  }
-})
+let lists = []
